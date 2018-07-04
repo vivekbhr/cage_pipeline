b5ce60cefc57aebcbc23d13e5cbeb64c04afb074
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2018-07-04 10:53:24 +0200

    renamed Snakefile, fixed demult script

--- rscripts/demult.R
+++ rscripts/demult.R
@@ -4,16 +4,17 @@
 # Arg 2,3 : R1 and R2 for de-multiplexing
 # Arg 4 : tsv file with barcodes (col1) and sample names(col2)
 # Arg 5 : output dir
+# Arg 6 : No. of cores
 library(icetea)
 args<-commandArgs(TRUE)
 df <- read.delim(args[4], stringsAsFactors = FALSE)
 
-colnames(df) <- c("barcode", "sample")
+colnames(df) <- c("barcode", "sample", "group")
 cs <- newCapSet(expMethod = args[1],
                 fastq_R1 = args[2],
                 fastq_R2 = args[3],
                 idxList = df$barcode,
                 sampleNames = df$sample)
 
-cs <- demultiplexFASTQ(cs, outdir = args[5], max_mismatch = 1, ncores = 10)
+cs <- demultiplexFASTQ(cs, outdir = args[5], max_mismatch = 1, ncores = args[6])
 save(cs, file = file.path(args[5], "CSobject.Rdata"))
